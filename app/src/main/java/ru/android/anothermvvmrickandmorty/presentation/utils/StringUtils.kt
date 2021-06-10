51c55f955374559dafb2eb3ac57ef7111d1bc727
VMakhota <klemeo@icloud.com>
2021-06-10 11:04:55 +0300

    added utils

---
+++ app/src/main/java/ru/android/anothermvvmrickandmorty/presentation/utils/StringUtils.kt
@@ -0,0 +1,26 @@
+package ru.android.anothermvvmrickandmorty.presentation.utils
+
+fun String.pageCharacters(): Int = this.replace(
+    "https://rickandmortyapi.com/api/character?page=",
+    ""
+).toInt()
+
+fun String.pageEpisodes(): Int = this.replace(
+    "https://rickandmortyapi.com/api/episode?page=",
+    ""
+).toInt()
+
+fun String.pageLocations(): Int = this.replace(
+    "https://rickandmortyapi.com/api/location?page=",
+    ""
+).toInt()
+
+fun String.pageCharacter(): Int = this.replace(
+    "https://rickandmortyapi.com/api/character/",
+    ""
+).toInt()
+
+fun String.pageEpisode(): Int = this.replace(
+    "https://rickandmortyapi.com/api/episode/",
+    ""
+).toInt()
