ec9d62c7e609ad6d50347b5a45c5f1aac243525b
VMakhota <klemeo@icloud.com>
2021-06-10 10:13:32 +0300

    added main activity contract

---
+++ app/src/main/java/ru/android/anothermvvmrickandmorty/presentation/MainActivityContract.kt
@@ -0,0 +1,42 @@
+package ru.android.anothermvvmrickandmorty.presentation
+
+interface MainActivityContract : CharactersScreen, CharacterScreenOne, CharacterScreenTwo,
+    LocationsScreen,
+    LocationScreenOne, LocationScreenTwo,
+    EpisodesScreen, EpisodeScreenOne, EpisodeScreenTwo
+
+interface CharactersScreen {
+    fun openCharactersScreen()
+}
+
+interface CharacterScreenOne {
+    fun openCharacterScreenOne(id: Int)
+}
+
+interface CharacterScreenTwo {
+    fun openCharacterScreenTwo(id: Int)
+}
+
+interface LocationsScreen {
+    fun openLocationsScreen()
+}
+
+interface LocationScreenOne {
+    fun openLocationScreenOne(id: Int)
+}
+
+interface LocationScreenTwo {
+    fun openLocationScreenTwo(id: Int)
+}
+
+interface EpisodesScreen {
+    fun openEpisodesScreen()
+}
+
+interface EpisodeScreenOne {
+    fun openEpisodeScreenOne(id: Int)
+}
+
+interface EpisodeScreenTwo {
+    fun openEpisodeScreenTwo(id: Int)
+}
