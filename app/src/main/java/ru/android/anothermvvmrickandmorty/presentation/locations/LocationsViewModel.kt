7dd14a3e3dac38ed751901a9f577d7f98962e6b8
VMakhota <klemeo@icloud.com>
2021-06-10 10:29:44 +0300

    added locations view model

---
+++ app/src/main/java/ru/android/anothermvvmrickandmorty/presentation/locations/LocationsViewModel.kt
@@ -0,0 +1,38 @@
+package ru.android.anothermvvmrickandmorty.presentation.locations
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import ru.android.anothermvvmrickandmorty.base.ResultObserverDelegate
+import ru.android.anothermvvmrickandmorty.domain.locations.LocationsInteractor
+import ru.android.anothermvvmrickandmorty.domain.models.location_body.Location
+
+class LocationsViewModel(
+    private val locationsInteractor: LocationsInteractor
+) : ViewModel() {
+
+    var location = MutableLiveData<Location?>()
+
+    private var getLocationsObserver = Observer<Result<Location>> { result ->
+        handleCategoryResult(result)
+    }
+
+    private var getLocationsLiveData: LiveData<Result<Location>>?
+            by ResultObserverDelegate(getLocationsObserver)
+
+    fun getLocations(page: Int? = null) {
+        getLocationsLiveData = locationsInteractor.getLocations(page)
+    }
+
+    private fun handleCategoryResult(result: Result<Location>) {
+        result
+            .onSuccess {
+                location.postValue(it)
+            }
+            .onFailure {
+
+            }
+    }
+
+}
