1cf4c5d0185323e3cff316e5de59fdf6f977b653
VMakhota <klemeo@icloud.com>
2021-06-12 11:36:21 +0300

    added character view model

---
+++ app/src/main/java/ru/android/anothermvvmrickandmorty/presentation/character/CharacterViewModel.kt
@@ -0,0 +1,39 @@
+package ru.android.anothermvvmrickandmorty.presentation.character
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import ru.android.anothermvvmrickandmorty.base.ResultObserverDelegate
+import ru.android.anothermvvmrickandmorty.domain.character.CharacterInteractor
+import ru.android.anothermvvmrickandmorty.domain.models.character_body.CharacterResult
+
+class CharacterViewModel(
+    private val characterInteractor: CharacterInteractor
+) : ViewModel() {
+
+    var character = MutableLiveData<CharacterResult?>()
+
+    private var getCharacterObserver = Observer<Result<CharacterResult>> { result ->
+        handleCategoryResult(result)
+    }
+
+    private var getCharacterLiveData: LiveData<Result<CharacterResult>>?
+            by ResultObserverDelegate(getCharacterObserver)
+
+    fun getCharacter(id: Int) {
+        getCharacterLiveData = characterInteractor.getCharacter(id)
+    }
+
+    private fun handleCategoryResult(result: Result<CharacterResult>) {
+        result
+            .onSuccess {
+                character.postValue(it)
+            }
+            .onFailure {
+
+            }
+
+    }
+
+}
