214d5b287774c936930d8fa2c47d3947d1a0a3bd
VMakhota <klemeo@icloud.com>
2021-06-15 12:16:34 +0300

    added display of the list of locations

--- app/src/main/java/ru/android/anothermvvmrickandmorty/presentation/locations/LocationsFragment.kt
+++ app/src/main/java/ru/android/anothermvvmrickandmorty/presentation/locations/LocationsFragment.kt
@@ -1,28 +1,83 @@
 package ru.android.anothermvvmrickandmorty.presentation.locations
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.fragment_locations.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.android.anothermvvmrickandmorty.R
+import ru.android.anothermvvmrickandmorty.base.FragmentListenerUtils
 import ru.android.anothermvvmrickandmorty.databinding.FragmentLocationsBinding
+import ru.android.anothermvvmrickandmorty.presentation.LocationScreenOne
 
 class LocationsFragment : Fragment() {
+
+    private val viewModel: LocationsViewModel by viewModel()
+
+    private lateinit var locationListener: LocationScreenOne
+
+    private val locationsAdapter = LocationsAdapter(LocationsListener {
+
+    })
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        locationListener =
+            FragmentListenerUtils.getFragmentListener(this, LocationScreenOne::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val binding = DataBindingUtil.inflate<FragmentLocationsBinding>(
-            inflater,
-            R.layout.fragment_locations,
-            container,
-            false
-        )
-        return binding.root
+    ): View = DataBindingUtil.inflate<FragmentLocationsBinding>(
+        inflater,
+        R.layout.fragment_locations,
+        container,
+        false
+    ).apply {
+        viewModel = this@LocationsFragment.viewModel
+        lifecycleOwner = viewLifecycleOwner
+
+    }
+        .root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        observeViewModel()
+    }
+
+    private fun initView() {
+        viewModel.getLocations()
+
+        with(recyclerView) {
+            val manager = GridLayoutManager(activity, 1)
+            layoutManager = manager
+            adapter = locationsAdapter
+        }
+
+        buttonBack.setOnClickListener {
+            activity?.onBackPressed()
+        }
+
+    }
+
+    private fun observeViewModel() {
+        viewModel.location.observe(viewLifecycleOwner, {
+            if (it != null) {
+                it.results?.let { results -> locationsAdapter.addHeaderAndSubmitList(results) }
+                pbPost.isGone = true
+                backButton.isGone = viewModel.visiblePrev()
+                nextButton.isGone = viewModel.visibleNext()
+            }
+        })
     }
 
 }
--- app/src/main/java/ru/android/anothermvvmrickandmorty/presentation/locations/LocationsViewModel.kt
+++ app/src/main/java/ru/android/anothermvvmrickandmorty/presentation/locations/LocationsViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import ru.android.anothermvvmrickandmorty.base.ResultObserverDelegate
 import ru.android.anothermvvmrickandmorty.domain.locations.LocationsInteractor
 import ru.android.anothermvvmrickandmorty.domain.models.location_body.Location
+import ru.android.anothermvvmrickandmorty.presentation.utils.pageLocations
 
 class LocationsViewModel(
     private val locationsInteractor: LocationsInteractor
@@ -33,6 +34,27 @@ class LocationsViewModel(
             .onFailure {
 
             }
+    }
+
+
+    fun flippingNext() {
+        val nextPage = location.value?.info?.next?.pageLocations()
+        if (nextPage != null) getLocations(nextPage)
+    }
+
+    fun flippingPrev() {
+        val prevPage = location.value?.info?.prev?.toString()?.pageLocations()
+        if (prevPage != null) getLocations(prevPage)
+    }
+
+    fun visiblePrev(): Boolean {
+        val prevPage = location.value?.info?.prev?.toString()?.pageLocations()
+        return prevPage == null
+    }
+
+    fun visibleNext(): Boolean {
+        val nextPage = location.value?.info?.next?.pageLocations()
+        return nextPage == null
     }
 
 }
