3a1d00c4f1a4024f2a3575078da404f1c4141cbd
VMakhota <klemeo@icloud.com>
2021-06-21 14:51:31 +0300

    added adapter to snippet

--- app/src/main/java/ru/android/anothermvvmrickandmorty/presentation/character/CharacterFragment.kt
+++ app/src/main/java/ru/android/anothermvvmrickandmorty/presentation/character/CharacterFragment.kt
@@ -5,15 +5,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.fragment_character.*
+import kotlinx.android.synthetic.main.fragment_character.buttonBack
+import kotlinx.android.synthetic.main.fragment_character.recyclerView
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.android.anothermvvmrickandmorty.R
 import ru.android.anothermvvmrickandmorty.base.FragmentListenerUtils
 import ru.android.anothermvvmrickandmorty.databinding.FragmentCharacterBinding
 import ru.android.anothermvvmrickandmorty.presentation.EpisodeScreenTwo
+import ru.android.anothermvvmrickandmorty.presentation.number.NumberListener
 
 class CharacterFragment : Fragment() {
 
@@ -22,6 +27,10 @@ class CharacterFragment : Fragment() {
     private lateinit var episodeListener: EpisodeScreenTwo
 
     private val navArgs by navArgs<CharacterFragmentArgs>()
+
+    private val episodeAdapter = EpisodeAdapter(NumberListener {
+        episodeListener.openEpisodeScreenTwo(it.toInt())
+    })
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -55,6 +64,12 @@ class CharacterFragment : Fragment() {
     private fun initView() {
         viewModel.getCharacter(navArgs.id)
 
+        with(recyclerView) {
+            val manager = GridLayoutManager(activity, 5)
+            layoutManager = manager
+            adapter = episodeAdapter
+        }
+
         buttonBack.setOnClickListener {
             activity?.onBackPressed()
         }
@@ -63,7 +78,8 @@ class CharacterFragment : Fragment() {
 
     private fun observeViewModel() {
         viewModel.character.observe(viewLifecycleOwner, {
-
+            it?.episode?.let { episodeList -> episodeAdapter.addHeaderAndSubmitList(episodeList) }
+            pbPost.isGone = true
         })
     }
 
