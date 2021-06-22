0554da219e5abd8a7299a8812a8b502973f11776
VMakhota <klemeo@icloud.com>
2021-06-22 17:50:45 +0300

    added adapter to snippet

--- app/src/main/java/ru/android/anothermvvmrickandmorty/presentation/episode/EpisodeFragment.kt
+++ app/src/main/java/ru/android/anothermvvmrickandmorty/presentation/episode/EpisodeFragment.kt
@@ -5,15 +5,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.fragment_episode.*
 import ru.android.anothermvvmrickandmorty.R
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.android.anothermvvmrickandmorty.base.FragmentListenerUtils
 import ru.android.anothermvvmrickandmorty.databinding.FragmentEpisodeBinding
 import ru.android.anothermvvmrickandmorty.presentation.CharacterScreenTwo
+import ru.android.anothermvvmrickandmorty.presentation.number.NumberListener
 
 class EpisodeFragment : Fragment() {
 
@@ -22,6 +25,10 @@ class EpisodeFragment : Fragment() {
     private lateinit var characterListener: CharacterScreenTwo
 
     private val navArgs by navArgs<EpisodeFragmentArgs>()
+
+    private val characterAdapter = CharacterAdapter(NumberListener {
+        characterListener.openCharacterScreenTwo(it.toInt())
+    })
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -54,6 +61,12 @@ class EpisodeFragment : Fragment() {
     private fun initView() {
         viewModel.getEpisode(navArgs.id)
 
+        with(recyclerView) {
+            val manager = GridLayoutManager(activity, 5)
+            layoutManager = manager
+            adapter = characterAdapter
+        }
+
         buttonBack.setOnClickListener {
             activity?.onBackPressed()
         }
@@ -62,7 +75,10 @@ class EpisodeFragment : Fragment() {
 
     private fun observeViewModel() {
         viewModel.episode.observe(viewLifecycleOwner, {
-
+            if (it != null) {
+                it.characters?.let { characterList -> characterAdapter.addHeaderAndSubmitList(characterList) }
+                pbPost.isGone = true
+            }
         })
     }
 
